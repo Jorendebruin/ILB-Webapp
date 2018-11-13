d5daea8216d297fb43ed4c7c90973d01eef01257
sander boer <mail@sanderboer.nl>
2018-11-13 10:06:19 +0100

    Scaffold react gulp app

---
+++ app/components/home/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+  
+  render() {
+    return (
+      <div>
+        <h2>Home</h2>
+        <p>
+          Have fun working on ILB-ec2-controller! &lt;3
+        </p>
+      </div>
+    );
+  }
+}
