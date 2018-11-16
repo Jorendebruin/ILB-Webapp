4a50d2df583402ac8cf3bad11257bf56bb8b6054
sander boer <mail@sanderboer.nl>
2018-11-16 14:23:49 +0100

    Added start / stop functionality

--- app/components/instance/instance.js
+++ app/components/instance/instance.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
+import axios from 'axios';
+
 import {
   MdLocationOn,
   MdWatchLater,
   MdCloud,
   MdHealing,
   MdExplore,
-  MdPlayArrow
+  MdPlayArrow,
+  MdPause,
+  MdUpdate,
+  MdStop,
+  MdGroupWork
 } from 'react-icons/md';
 
 export default class Home extends React.Component  {
@@ -17,13 +23,45 @@ export default class Home extends React.Component  {
     }
   }
 
+  updateInstance() {
+    this.setState({
+      instance: this.state.instance
+    });
+  }
+
+  toggleInstanceState() {
+    const gateway_url = "https://gq4yjqab1g.execute-api.eu-west-1.amazonaws.com/TEST/";
+    var state = this.state.instance.instance.state;
+
+    // You can only toggle the state when it's either running (16) or stopped (80)
+    if(state != 16 && state != 80) return;
+
+    this.state.instance.instance.state = state == 16 ? 32 : 0;
+    this.updateInstance();
+
+    axios.get(gateway_url + (state == 16 ? 'stop' : 'start'), {
+      params: {
+        ID: this.state.instance.metadata.instanceId
+      }
+    })
+    .then((response) => {
+      console.log('finished', response);
+    })
+    .catch((err) => {
+      console.log('error', error);
+    });
+  }
+
   render() {
     var instanceState;
+    var buttonDisabled = true;
     switch (this.state.instance.instance.state) {
-      case 2:
+      case 16:
+        buttonDisabled = false;
         instanceState = 'state--ok';
         break;
-      case 4:
+      case 80:
+        buttonDisabled = false;
         instanceState = 'state--error';
         break;
       default:
@@ -47,6 +85,12 @@ export default class Home extends React.Component  {
         break;
     }
 
+    var timeDiff = new Date(new Date() - new Date(this.state.instance.instance.startuptime));
+    var runTime = {
+      hours: timeDiff.getHours(),
+      minutes: timeDiff.getMinutes()
+    };
+
     return (
       <div className={ `c-instance ${environment}` }>
         <header>
@@ -60,30 +104,43 @@ export default class Home extends React.Component  {
             {this.state.instance.location.location}
           </li>
           <li>
-            <MdCloud />
+            <MdGroupWork />
             {this.state.instance.location.availabilityZone}
           </li>
           <li className={instanceState}>
             <MdExplore />
-            {this.state.instance.instance.state == 1 ? 'starting up' : null}
-            {this.state.instance.instance.state == 2 ? 'running' : null}
-            {this.state.instance.instance.state == 3 ? 'shutting down' : null}
-            {this.state.instance.instance.state == 4 ? 'stopped' : null}
+            {this.state.instance.instance.state == 0 ? 'pending' : null}
+            {this.state.instance.instance.state == 16 ? 'running' : null}
+            {this.state.instance.instance.state == 32 ? 'shutting-down' : null}
+            {this.state.instance.instance.state == 48 ? 'terminated' : null}
+            {this.state.instance.instance.state == 64 ? 'stopping' : null}
+            {this.state.instance.instance.state == 80 ? 'stopped' : null}
           </li>
           <li className={this.state.instance.status.health.passed < this.state.instance.status.health.amount ? 'state--warning' : 'state--ok'}>
             <MdHealing />
             {this.state.instance.status.health.passed}/{this.state.instance.status.health.amount}
-            &nbsp; checks
+            &nbsp;checks
           </li>
           <li>
             <MdWatchLater />
-            02h 32m
+            { this.state.instance.instance.state == 16 ? runTime.hours+'h '+runTime.minutes+'m' : '-'}
           </li>
         </ul>
 
-        <button>
-          <MdPlayArrow />
-          Start instance
+        <button disabled={buttonDisabled} onClick={() => this.toggleInstanceState() }>
+          {this.state.instance.instance.state == 0 ? <MdUpdate /> : null}
+          {this.state.instance.instance.state == 16 ? <MdPause /> : null}
+          {this.state.instance.instance.state == 32 ? <MdUpdate /> : null}
+          {this.state.instance.instance.state == 48 ? <MdStop /> : null}
+          {this.state.instance.instance.state == 64 ? <MdUpdate /> : null}
+          {this.state.instance.instance.state == 80 ? <MdPlayArrow /> : null}
+
+          {this.state.instance.instance.state == 0 ? 'Wating for response' : null}
+          {this.state.instance.instance.state == 16 ? 'Stop instance' : null}
+          {this.state.instance.instance.state == 32 ? 'Wating for response' : null}
+          {this.state.instance.instance.state == 48 ? 'Instance is terminated' : null}
+          {this.state.instance.instance.state == 64 ? 'Wating for response' : null}
+          {this.state.instance.instance.state == 80 ? 'Start instance' : null}
         </button>
       </div>
     )
