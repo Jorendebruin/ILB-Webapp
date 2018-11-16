70b7ac96db71cca5828ca77cabf599389377c7ee
marsmar1 <raymon.marsman@hva.nl>
2018-11-16 16:23:21 +0100

    Initiele setup van de instance overview

---
+++ app/components/instance/instanceOverview.js
@@ -0,0 +1,87 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+            instance: props.instance,
+            showPopup: false
+        }
+    }
+
+    togglePopup() {
+        this.setState({
+            showPopup: !this.state.showPopup
+        });
+    }
+
+    render() {
+        var instanceState;
+        switch (this.state.instance.instance.state) {
+            case 2:
+                instanceState = 'state--ok';
+                break;
+            case 4:
+                instanceState = 'state--error';
+                break;
+            default:
+                instanceState = 'state--warning';
+                break;
+        }
+
+        var environment;
+        switch (this.state.instance.location.environment) {
+            case 1:
+                environment = 'environment--o';
+                break;
+            case 2:
+                environment = 'environment--t';
+                break;
+            case 3:
+                environment = 'environment--a';
+                break;
+            case 4:
+                environment = 'environment--p';
+                break;
+        }
+
+        return (
+            <div className="detailOverview">
+                <div className="regionBar">
+                </div>
+                <div className="dataOverview">
+                    <h1>Meta Data</h1>
+                    <p>Name:</p>
+                    <p>Instance ID</p>
+                    <br></br>
+                    <h1>Location</h1>
+                    <p>Regio</p>
+                    <p>Omgeving</p>
+                    <p>Status</p>
+                    <br></br>
+                    <h1>Instance</h1>
+                    <p>Instance type</p>
+                    <p>Runtime</p>
+                    <p>Availability zone</p>
+                    <br></br>
+                    <h1>Status</h1>
+                    <p>Health</p>
+                    <p>Alarm</p>
+                </div>
+                <div className="logs">
+                    <h1>Log</h1>
+                    <table>
+                        <th>
+                            <tr></tr>
+                            <tr></tr>
+                            <tr></tr>
+                        </th>
+                        <td>
+                            <tr></tr>
+                        </td>
+                    </table>
+                </div>
+            </div>
+        );
+    }
+}
