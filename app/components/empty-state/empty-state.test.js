86fa1d6f2affbf32c34e67ea7df42911126a3cae
sander boer <mail@sanderboer.nl>
2018-11-26 12:18:38 +0100

    A bit more advanced testing

--- app/components/empty-state/empty-state.test.js
+++ app/components/empty-state/empty-state.test.js
@@ -6,3 +6,21 @@ import EmptyState from './empty-state';
 it("renders without crasching", () => {
   shallow(<EmptyState />);
 });
+
+it("should render the title", () => {
+  let title = "Test";
+
+  const wrapper = shallow(<EmptyState title={title} />);
+  const element = wrapper.find('.c-empty-state__title').first();
+
+  expect(element.text()).toEqual(title);
+});
+
+it("should render the subtitle", () => {
+  let subtitle = "test";
+
+  const wrapper = shallow(<EmptyState subtitle={subtitle} />);
+  const element = wrapper.find('.c-empty-state__subtitle').first();
+
+  expect(element.text()).toEqual(subtitle);
+});
