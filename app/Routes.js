d49be4b08a6d3e8b739456935c3f93de22cff56d
mcbx <markbobeldijk@gmail.com>
2018-12-11 10:41:25 +0100

    modified routes / no login

--- app/Routes.js
+++ app/Routes.js
@@ -8,7 +8,12 @@ import UnauthenticatedRoute from "./UnauthenticatedRoute";
 
 export default ({ childProps }) =>
   <Switch>
-    <AuthenticatedRoute path="/" exact component={Home} props={childProps} />
-      <UnauthenticatedRoute path="/login" exact component={Login} props={childProps} />
+
+    // Verwijder deze route voor authenticatie en Login, en uncomment de lines hieronder
+    <Route exact path="/" component={Home} props={childProps}  />
+
+    // Uncomment de volgende lines voor authenticatie/login
+    // <AuthenticatedRoute exact path="/" component={Home} props={childProps} />
+      // <UnauthenticatedRoute exact path="/login" component={Login} props={childProps} />
     <Route component={NotFound} />
   </Switch>;
