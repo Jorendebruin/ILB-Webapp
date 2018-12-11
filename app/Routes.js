85c4d44ed89d322c64a6c68bccc16d97405da52d
mcbx <markbobeldijk@gmail.com>
2018-12-11 16:19:57 +0100

    routes modded

--- app/Routes.js
+++ app/Routes.js
@@ -9,11 +9,11 @@ import UnauthenticatedRoute from "./UnauthenticatedRoute";
 export default ({ childProps }) =>
   <Switch>
 
-    // Verwijder deze route voor authenticatie en Login, en uncomment de lines hieronder
+    {/* // Verwijder deze route voor authenticatie en Login, en uncomment de lines hieronder */}
     <Route exact path="/" component={Home} props={childProps}  />
 
-    // Uncomment de volgende lines voor authenticatie/login
-    // <AuthenticatedRoute exact path="/" component={Home} props={childProps} />
-      // <UnauthenticatedRoute exact path="/login" component={Login} props={childProps} />
+    {/* // Uncomment de volgende lines voor authenticatie/login */}
+    {/* // <AuthenticatedRoute exact path="/" component={Home} props={childProps} /> */}
+      {/* // <UnauthenticatedRoute exact path="/login" component={Login} props={childProps} /> */}
     <Route component={NotFound} />
   </Switch>;
